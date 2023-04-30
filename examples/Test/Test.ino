c4b3fd44379ecbee818a9ac8bd8e2ef8d5339036
soylentOrange <soylentOrange@users.noreply.github.com>
2023-04-30 06:15:22 +0200

    Update Test.ino

--- examples/Test/Test.ino
+++ examples/Test/Test.ino
@@ -1,36 +1,51 @@
 
-/* Forced-BME280 Library
+/* Forced-BMX280 Library
+   soylentOrange - https://github.com/soylentOrange/Forced-BMX280
+
+   forked from:
+   Forced-BME280 Library
    Jochem van Kranenburg - jochemvk.duckdns.org - 9 March 2020
 */
 
-#include <forcedClimate.h>
+#include <TinyWireM.h>      // https://github.com/adafruit/TinyWireM
+#include <forcedBMX280.h>
 
-// Create an instance of a climate sensor. The parameters I2C bus and I2C address are optional. For example:
-// - ForcedClimate climateSensor = ForcedClimate(Wire, 0x76);
-// - ForcedClimate climateSensor = ForcedClimate(TinyWireM, 0x77);
-ForcedClimate climateSensor = ForcedClimate();
+// Create an instance of a BMX280. The parameters I2C bus and I2C address are optional. For example:
+// if a BMP280 is connected or you don't care about humidity:
+// - ForcedBMX280 climateSensor = ForcedBMP280(Wire, 0x76);
+// - ForcedBMX280 climateSensor = ForcedBMP280(TinyWireM, 0x77);
+
+// if a BME280 is connected; will allow humidity measurement
+// - ForcedBMX280 climateSensor = ForcedBME280(Wire, 0x76);
+
+
+// BME280-sensor
+#define BME_ADDRESS 0x76  // I2C-Address of sensor
+ForcedBMX280 climateSensor = ForcedBME280(TinyWireM, BME_ADDRESS);
 
 void setup(){
 	Serial.begin(9600);
-	climateSensor.begin();
+	TinyWireM.begin(); 				// Or "Wire.begin()" when not using an ATtiny.
+	
+	// you might want to check if the sensor is available when "starting it up"
+	while(climateSensor.begin()) {
+	  delay(100);
+	} 
 }
 
 void loop(){
 	climateSensor.takeForcedMeasurement();
-	// Instead of "takeForcedMeasurement();" one can also use one of getTemperature(true), 
-	// getHumidity(true) or getPressure(true) to perform a forced measurement.
-	
 	Serial.print("Temperature: ");
-	Serial.print(climateSensor.getTemperatureCelcius());
+	Serial.print(climateSensor.getTemperatureCelsius());
 	Serial.print(", Humidity: ");
 	Serial.print(climateSensor.getRelativeHumidity());
 	Serial.print(" and Pressure: ");
 	Serial.print(climateSensor.getPressure());
 	Serial.println();
 	delay(1000);
-	Serial.print("Only temperature: ");
 	// Perform measurement integrated getTemperature(); useful for when only one value has to be used.
-	Serial.print(climateSensor.getTemperatureCelcius(true));
+	Serial.print(climateSensor.getTemperatureCelsius(true));
 	Serial.println();
 	delay(5000);
 }
+
